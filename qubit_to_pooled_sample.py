ffea0b69822312552cb6100c11a4329cfe81acb8
SSmith-NUIG <stephensmith@live.ie>
2020-10-01 12:19:31 +0100

    Update qubit_to_pooled_sample.py

--- qubit_to_pooled_sample.py
+++ qubit_to_pooled_sample.py
@@ -3,14 +3,14 @@ import numpy as np
 import sys
 
 #setting up test dataframe (only run this once)
-#df = pd.DataFrame({"number":[1,2,3,4,5,6], "conc":[35,54,29,33,43,35], "volume":[35,35,40,40,33,30]})
+#df = pd.DataFrame({"number":[1,2,3,4,5,6], "conc":[35,54,29,33,43,35], "volume":[35,35,35,35,35,35]})
 #df.to_csv("colony_test.csv", index=False)
 #df
 
 def  how_much_to_take(dataframe):
     #set up the dataframes columns
     dataframe["total_dna"] = dataframe.apply(lambda row: row.conc*row.volume, axis=1)    
-    dataframe["ratio"] = dataframe["conc"].apply( lambda x: 1/x)
+    dataframe["one_over_conc"] = dataframe["conc"].apply( lambda x: 1/x)
     dataframe["take"] = np.nan
     dataframe["group"] = np.nan
     
@@ -26,18 +26,12 @@ def  how_much_to_take(dataframe):
 
             group +=1
     
-    #the 35 here is the volume of the DNA samples, this may change and might be edited to a sys.argv variable
+    #the 35 here is the volume of final DNA tube, this may change and might be edited to a sys.argv variable
     for m in range(1, dataframe["group"].nunique()+1):
-        dataframe["take"] = dataframe["ratio"] * (35 / sum(dataframe.loc[dataframe['group'] == m]["ratio"]))
+        dataframe["take"] = dataframe["one_over_conc"] * (35 / sum(dataframe.loc[dataframe['group'] == m]["one_over_conc"]))
                 
     print(dataframe)
 
 df = pd.read_csv(sys.argv[1])
-
-#this if else was only for testing
-if len(sys.argv) >2:
-    df= df.assign(volume=int(sys.argv[2]))
-else:
-    pass
 
 how_much_to_take(df)
