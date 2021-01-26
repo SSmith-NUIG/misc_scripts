c412449a57c454c49a1f59a1717e3d71d63bd6f4
SSmith-NUIG <stephensmith@live.ie>
2021-01-26 15:15:17 +0000

    Update qubit_to_pooled_sample.py

--- qubit_to_pooled_sample.py
+++ qubit_to_pooled_sample.py
@@ -1,15 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Sep 23 10:05:42 2020
-
 @author: stephen
 """
+
 import pandas as pd
 import argparse
 import textwrap
 import numpy as np
-#import sys
 
 parser = argparse.ArgumentParser(description='pool_dna_script')
 
@@ -25,52 +22,72 @@ parser.add_argument('-poolsize', metavar='-s', nargs='+', default="6",
                     help= textwrap.dedent('''\
                                           Enter the number of samples you are pooling together, default is 6'''))
 
+parser.add_argument('-outfile', metavar='-o', nargs='+', default="colony_pool_output.csv",
+                    help= textwrap.dedent('''\
+                                          Enter a filename to write results to, default is colony_pool_output.csv'''))
 
 options = parser.parse_args()
-#setting up test dataframe (only run this once)
-#df = pd.DataFrame({"number":[1,2,3,4,5,6], "conc":[35,54,29,33,43,35], "volume":[35,35,40,40,33,30]})
-#df.to_csv("colony_test.csv", index=False)
-#df
 
 def  how_much_to_take(dataframe):
     
-    dataframe.rename(columns={"Original sample conc.":"conc"}, inplace=True)
-    dataframe["volume"] = float(options.volume[0])
-    #dataframe.volume = dataframe.volume.astype(float)
-    dataframe["total_dna"] = dataframe.apply(lambda row: row.conc*row.volume, axis=1)    
-    dataframe["inverse_conc"] = dataframe["conc"].apply(lambda x: 1/x)
-    dataframe["group"] = np.nan
-    dataframe["take"] = np.nan
-    dataframe = dataframe.iloc[::-1]
-    new_index = list(range(1,len(dataframe["volume"])+1))
-    #dataframe.reset_index(inplace=True)
+    dataframe.rename(columns={"Original sample conc.":"Concentration (ng/microL)"}, inplace=True)
+    dataframe["Volume (microL)"] = float(options.volume[0])
+    dataframe["Total DNA (ng)"] = dataframe["Concentration (ng/microL)"] * dataframe["Volume (microL)"] 
+    dataframe["Inverse Concentration"] = 1/dataframe["Concentration (ng/microL)"]
+    dataframe["Pool Number"] = np.nan
+    dataframe["Volume of sample in pool (microL)"] = np.nan
+    dataframe["DNA Contributed (ng)"]= np.nan
     
-    current_group = 1
+    #The first row of the Qubit output csv file is the last sample that
+    #was analysed, this is counter intuitive to analysis as you would
+    #expect the first row to be the first sample analysed so the next line
+    #reverses the order of the rows in the Qubit output csv.
+    dataframe = dataframe.iloc[::-1].reset_index(drop=True)
     
-
-    
+    #make sure that pool_size input is a integer
+    #and the volume input is a float (e.g could have 35.5microL )
     pool_size = int(options.poolsize[0])
     vol = float(options.volume[0])
     
-    for i in range(0, len(dataframe["Run ID"])):
-        if i%pool_size==0:
-            for j in range(0, pool_size):           
-                dataframe["group"][i+j] = current_group
-            current_group +=1
+    #split the dataframe by the size of your pools
+    #e.g if Pool size is 6 and the dataframe has 12 Qubit entries
+    #this command will get the first entry of both pools and save them to a dataframe
+    pool_starts = dataframe[::pool_size]
+    pool_starts = pool_starts.copy()
     
-    group_counter = 0
-     
-    for m in range(1, dataframe["group"].nunique()+1):
-        for n in range(group_counter, len(dataframe["inverse_conc"])):
-            dataframe["take"][n] = dataframe["inverse_conc"][n] * (vol / sum(dataframe.loc[dataframe['group'] == m]["inverse_conc"]))
-        group_counter += len(dataframe[dataframe["group"]==m])   
+    #assign values to each first entry of the pools starting from 1 to the length of 
+    #the number of pools in pool_starts
+    #e.g is there are two pools then they will get numbers 1 and 2
+    pool_starts["Pool Number"] = [i for i in range(1,len(pool_starts)+1)]
     
-    dataframe = dataframe.reindex(new_index, axis=1)
+    #updates the dataframe[Pool] values using the pool_starts[Pool] dataframe
+    dataframe["Pool Number"] = pool_starts["Pool Number"]
     
-    print(dataframe[["conc","total_dna","o", "group","take"]])
+    #forward fill the rest of the dataframe[Pool] column.
+    #this will fill the NaNs from one Pool number to the next using the value
+    #given to the group in pool_starts["Pool Number"] = [i for i in range(1,len(pool_starts)+1)]
+    dataframe["Pool Number"].ffill(inplace=True)
 
+    #multiply the values in the inverse_conc column with the desired Pooled sample volume
+    #divided by the sum of the inverse concentration of that Pool to get how much
+    #volume of each sample you will contribute to the Pooled sample
+    dataframe["Volume in pool (microL)"] = dataframe["Inverse Concentration"] * vol/dataframe.groupby("Pool Number")["Inverse Concentration"].transform("sum")
+    
+    #calculate how much dna each sample is contributing to each Pool
+    #each value should be idential within pools
+    dataframe["DNA Contributed (ng)"] = dataframe["Volume in pool (microL)"] * dataframe["Concentration (ng/microL)"]
+
+    #python indexes start at 0 which will not match the samples run
+    #this line reindexes the dataframe so that it starts at 1 for readability
+    dataframe.index= np.arange(1,(len(dataframe)+1))
+    dataframe["Sample Number"] = dataframe.index
+
+    headers= ["Pool Number","Volume (microL)","Concentration (ng/microL)","Total DNA (ng)","Inverse Concentration","Sample Number","Volume in pool (microL)","DNA Contributed (ng)"]
+
+    print(dataframe[headers])
+    print("Saving output to file: %s" % options.outfile[0])
+    with open(options.outfile[0], 'w') as output_file:
+        dataframe.to_csv(output_file, columns= headers, sep=',', encoding='utf-8', index=False)
 
 df = pd.read_csv(options.file[0])
-
-
 how_much_to_take(df)
