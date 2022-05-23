c2b324f89db800ae665c2fccb54e24ff4e48c25b
SSmith-NUIG <stephensmith@live.ie>
2022-05-23 12:39:29 +0100

    Create webscrape_apis_genes.py

---
+++ webscrape_apis_genes.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+opts = Options()
+opts.headless = True
+assert opts.headless  # Operating in headless mode
+driver = webdriver.Firefox(executable_path=r'/home/stephen/Downloads/geckodriver-v0.31.0-linux64/geckodriver')
+
+dfs = []
+for i in range(1,104):
+    url = "https://www.genscript.com/gene/7460/honey-bee-apis-mellifera?page="+str(i)
+    driver.get(url)
+    driver.implicitly_wait(5)
+    soup = BeautifulSoup(driver.page_source,'lxml')
+    tables = soup.find_all('table')
+    df1 = pd.read_html(str(tables))
+    df2 = pd.DataFrame(df1[0])
+    dfs.append(df2)
+
+combined = pd.concat(dfs)
+combined.reset_index(inplace=True)
+combined.drop("index", inplace=True, axis=1)
+combined.to_csv("/home/stephen/genes_to_fullname.csv", sep=",", index=False)
